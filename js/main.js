0a6baf02f7cfaad78258b70fabef73ac709bd93b
Avraam Georgiadis <Georgiadis.ab@gmail.com>
2016-11-16 04:31:01 +0100

    Working on the login of main page.

--- js/main.js
+++ js/main.js
@@ -1,36 +1,42 @@
 /*jslint browser: true*/
 /*global $, jQuery, alert*/
 //Send Post request to server with credentials 
-
-var $username = $('#fpUsername');
-var $password = $('#fpPassword');
+$(document).ready(function () {
+    "use strict";
+    var $username = $('#fpUsername');
+    var $password = $('#fpPassword');
 
 //Mount the onclick Function of Signin  
-$('#fpSignIN').on('click', function () {
-    "use strict";
-    var credentials = {
+    $('#fpSignIN').on('click', function () {
+    
+        var credentials = {
             username: $username.val(),
             password: $password.val(),
             grant_type: "password",
             scope: "mymanuals",
-            client_id: "clientapp"
+            client_id: "clientapp",
         };
     
     //Ajax call to the backend API
-    $.ajax({
-        type: 'POST',
-        url: 'http://localhost:8080/oauth/token',
-        
-        headers: {
-            "Accept":"application/json",
-            "Authorization": "Basic " + btoa(credentials.username + ":" + credentials.password)
-                 },
-        dataType: 'json',
-        data: JSON.stringify(credentials),
-        error: function () {
-            alert('Error pushing the credentials');
-        }
-    });
+        $.ajax({
+            type: 'POST',
+            url: 'http://localhost:8080/oauth/token',
+            crossOrigin: true,
+            cache: false,
+            header: ('Access-Control-Allow-Origin: *'),
+//            beforeSend: function (xhr) {
+//                xhr.setRequestHeader("Authorization", "Basic " + btoa(credentials.username + ":" + credentials.password));
+//            },
+            headers: {
+                "Accept": "application/json",
+                "Authorization": "Basic " + btoa($username + ":" + $password)
+            },
+            dataType: 'json',
+            data: JSON.stringify(credentials),
+            error: function () {
+                alert('Error pushing the credentials' + JSON.stringify(credentials));
+            }
+        });
     
     //Include the token for future calls.
     // beforeSend : function( xhr ) {
@@ -38,6 +44,6 @@ $('#fpSignIN').on('click', function () {
     //},
     
     
+    });
+
 });
-
-
