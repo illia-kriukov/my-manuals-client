c4d161440e98150afc70406bb8bf7f6589c53ff8
Ilya Kriukov <ilyakriukov@icloud.com>
2016-11-28 23:21:54 +0100

    Add Log In toastr pop up to all pages, fix footer, refactor project

--- js/main.js
+++ js/main.js
@@ -2,22 +2,27 @@
 /*global $, jQuery, alert*/
 //Send Post request to server with credentials
 $(document).ready(function () {
-    toastr.options = {              
-        "positionClass": "toast-bottom-right",
+    // Toastr notifications configuration
+    toastr.options = {
+        "positionClass": "toast-top-right",
         "onclick": null,
         "showDuration": "0",
         "hideDuration": "0",
         "timeOut": "5000",
         "showMethod": "fadeIn"
     };
+
+    // Consumer & Representative registration dropdown menu
     $('.dropdown').hover(function () {
         $(this).find('.dropdown-menu').stop(true, true).delay(100).fadeIn();
     }, function () {
         $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut();
     });
-    $('.dropdown-menu').hover(function () { $(this).stop(true, true); },
-      function () { $(this).stop(true, true).delay(200).fadeOut(); }
-    );
+    $('.dropdown-menu').hover(function () {
+        $(this).stop(true, true);
+    }, function () {
+        $(this).stop(true, true).delay(200).fadeOut();
+    });
     $("#close").click(function () {
         $("#popBackground").fadeOut();
         $("#popBox").fadeOut();
@@ -35,8 +40,8 @@ $(document).ready(function () {
     var $client_id = "mymanuals";
     var $secret = "secret";
 
-    //Mount the onclick Function of Signin
-    $('#fpSignIN').on('click', function () {        
+    // Mount the onclick Function of Sign In
+    $('#fpSignIN').on('click', function () {
         event.preventDefault();
         var credentials = new FormData();
         credentials.append('username', $username.val());
@@ -44,7 +49,7 @@ $(document).ready(function () {
         credentials.append('scope', $scope);
         credentials.append('grant_type', $grant_type);
 
-        //Ajax call to the backend API
+        // Ajax call to the backend API to receive Token
         $.ajax({
             type: 'POST',
             url: 'http://localhost:8080/oauth/token',
@@ -58,9 +63,10 @@ $(document).ready(function () {
             data: credentials,
             processData: false,
             contentType: false,
-            success: function (response) {                
+            success: function (response) {
                 window.localStorage.setItem('access_token', response.access_token);
-                //Ajax call to get user Role
+
+                // Ajax call to get user role
                 $.ajax({
                     type: 'GET',
                     url: 'http://localhost:8080/account/authorities',
@@ -78,14 +84,14 @@ $(document).ready(function () {
                     }
                 });
             },
-            error: function (xhr, status, error) {               
-                var jsonResponseText = $.parseJSON(xhr.responseText);              
+            error: function (xhr, status, error) {
+                var jsonResponseText = $.parseJSON(xhr.responseText);
                 $.each(jsonResponseText, function (name, val) {
-                    if (name == "error_description") {                        
+                    if (name == "error_description") {
                         toastr.error(val);
                     }
                 });
-            }                
+            }
         });
     });
 });
