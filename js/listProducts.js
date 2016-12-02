cb89ba1c64722f9a578db90a1eb36457dfb4fa13
DESKTOP-U4D216O\Imran Shokat SE <is222kh@student.lnu.se>
2016-12-02 20:40:04 +0500

    Filter Products by Category

--- js/listProducts.js
+++ js/listProducts.js
@@ -27,4 +27,41 @@ $(document).ready(function () {
             });
         }
     });
+    $('#btnfilter').on('click', function () {
+        //var valu = $("#category").val();
+        // console.log(valu);
+        if ($("#category").val() == "") {
+            toastr.warning("Please First Select Category");
+        }
+        else {
+            event.preventDefault();
+            //Ajax call to the backend API receive products
+            $.ajax({
+                type: 'GET',
+                url: 'http://localhost:8080/products?categories=' + $("#category").val() + '&page=0&count=10',
+                crossOrigin: true,
+                dataType: "JSON",
+                cache: false,
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                success: function (response) {
+                    console.log($("#category").val());
+                    var tableBody = $('#TableBody');
+                    tableBody.empty();
+                    $.each(response, function (index, product) {
+                        var tr = $('<tr/>');
+                        tableBody.append(tr);
+                        var td = $('<td>' + product.name + '</td>').appendTo(tr);
+                        var td = $('<td>' + product.model + '</td>').appendTo(tr);
+                        var td = $('<td>' + product.company.name + '</td>').appendTo(tr);
+                        var categoriesTd = $('<td/>').appendTo(tr);
+                        $.each(product.categories, function (index1, category) {
+                            var span = $('<span>' + " " + category.name + '</span>').appendTo(categoriesTd);
+                        });
+                    });
+                }
+            });
+        }
+    });
 });
